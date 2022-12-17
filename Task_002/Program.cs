ad26401c05cce28687121e15aee2b66a23229564
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-18 00:03:18 +0300

    Решил первую задачу

---
+++ Task_002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a = ");
+string userintA = Console.ReadLine() ?? "";
+int a = int.Parse(userintA);
+Console.WriteLine("Введите число b = ");
+string userintB = Console.ReadLine() ?? "";
+int b = int.Parse(userintB);
+if(a > b)
+{
+    Console.WriteLine("a = max ");
+}
+else
+{
+    Console.WriteLine("b = max");
+}

07614c844490dc6f2cba54233f0ecf33d55f0e91
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-17 22:20:06 +0300

    Создал небольшую программу

---
+++ Task_001/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Write("Как дела?:");
+string user = Console.ReadLine() ?? "";
+if (user.ToLower() == ("хорошо"))
+{
+    Console.WriteLine("Поздравляю!!!");
+
+    Console.Write("https://w7.pngwing.com/pngs/964/995/png-transparent-smiley-emoticon-thumb-signal-emoji-dank-face-smiley-thumb-signal-thumbnail.png");
+}
+else Console.WriteLine("Понятно");

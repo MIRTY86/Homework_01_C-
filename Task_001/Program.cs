6cff9117a34277ffc0833635a72c616542230dbb
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-17 22:39:57 +0300

    подправил первую программу

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -3,7 +3,7 @@ Console.Write("Как дела?:");
 string user = Console.ReadLine() ?? "";
 if (user.ToLower() == ("хорошо"))
 {
-    Console.WriteLine("Поздравляю!!!");
+    Console.WriteLine("Рад за тебя!!!");
 
     Console.Write("https://w7.pngwing.com/pngs/964/995/png-transparent-smiley-emoticon-thumb-signal-emoji-dank-face-smiley-thumb-signal-thumbnail.png");
 }

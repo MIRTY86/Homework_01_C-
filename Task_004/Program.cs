2d6367c78b39ef9d0c6aed3a027d4e086bd9065a
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-18 16:20:18 +0300

    Решил третью задачу

---
+++ Task_004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:  ");
+string usernumber = Console.ReadLine() ?? "";
+int num = int.Parse(usernumber);
+
+if(num % 2 == 0)
+{
+    Console.Write(num);
+    Console.WriteLine(" - четное число.");
+}
+else 
+{
+    Console.Write(num);
+    Console.WriteLine(" - нечетное число.");
+}

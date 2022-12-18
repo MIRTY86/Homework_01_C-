1279bffba6b7a09c1bcdafc255c7e63e4e017964
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-18 17:18:18 +0300

    Решил четвертую задачу

---
+++ Task_005/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число N:  ");
+string usernumber = Console.ReadLine() ?? "";
+int N = int.Parse(usernumber);
+int i = 0;
+Console.Write(N);
+Console.Write(" ---> ");
+while(i < N)
+{
+    if(i % 2 == 0)
+    {
+    Console.Write(i);
+    Console.Write(",");
+    }
+    i++;  
+    
+}
+ 

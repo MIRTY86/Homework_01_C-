bb488b2cc73bee95baff59091d00eb723b6f755d
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-24 00:34:26 +0300

    Задача -вывод третьей цифры слева, любого трехзначного числа

---
+++ Task_008/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string usernum = Console.ReadLine() ?? "";
+int N = int.Parse(usernum);
+int F = 0;
+
+if(N < 100) 
+{
+    Console.Write($"{N} - не трехзначное число!");
+  
+}
+else
+{
+    Console.Write($"{N}---->");
+    while(N > 999)
+        N = N / 10;
+
+    F = N  % 10;
+    Console.Write(F);
+}

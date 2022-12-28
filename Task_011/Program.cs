3f38014beebd5207c0cde2c64e17bcfb9f8460ae
Dmitriy Linov <Dimalinov@mail.ru>
2022-12-29 00:13:10 +0300

    Задача - выдача таблицы кубов всех чисел до N

---
+++ Task_011/Program.cs
@@ -0,0 +1,28 @@
+﻿double N;
+double i = 1;
+
+while (true)
+{
+    try
+    {
+        Console.Write("Введите число N: ");
+        string userN = Console.ReadLine() ?? "";
+        N = double.Parse(userN);
+        break;
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Введено неверное значение!{exc.Message}");
+    }
+
+}
+
+Console.Write($"{N} --->");
+
+while(i < N +1)
+{
+        double SecondN = Math.Pow(i , 3);
+        i ++;
+        Console.Write($" {SecondN}");
+}
+Console.Write(".");

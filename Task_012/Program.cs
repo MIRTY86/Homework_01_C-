5a868222aa90c892071204e7171f0436bc256684
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-04 22:19:55 +0300

    Пример кода конвертации строки в нецелое число, с применением метода изменения разделителя в числе!

---
+++ Task_012/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string num = "2.566";
+        //**********************************************************
+        //Далее пример измения разделителя в числе - запятую меняем на точку.
+        NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+        {
+            NumberDecimalSeparator = ".",
+        };
+
+        double n = Convert.ToDouble(num, numberFormatInfo );
+    }
+}

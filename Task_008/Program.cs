b9591dc18167975c499de9fb4f7523a0d9722f08
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-06 19:06:34 +0300

    Небольшая программка проверки равенства трех чисел через Bool переменную, с проверкой корректности ввода данных.

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -3,16 +3,16 @@ string usernum = Console.ReadLine() ?? "";
 int N = int.Parse(usernum);
 int F = 0;
 
-if((N < 100) && (N > -99))
+if ((N < 100) && (N > -99))
 {
     Console.Write($"{N} - не трехзначное число!");
 }
 else
 {
     Console.Write($"{N}---->");
-    while(N > 999)
+    while (N > 999)
         N = N / 10;
 
-    F = N  % 10;
+    F = N % 10;
     Console.Write(F);
 }
---
+++ Task_014/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Небольшая программка проверки равенства трех чисел через Bool переменную, с проверкой 
+корректности ввода данных. */
+int a;
+int b;
+int c;
+
+while (true)
+{
+    try
+    {
+        System.Console.WriteLine("Введите число a:");
+        a = int.Parse(Console.ReadLine() ?? "");
+        break;
+
+    }
+    catch (Exception exc)
+    {
+        System.Console.WriteLine($"Ошибка ввода данных!!! {exc.Message}");
+    }
+}
+while (true)
+{
+    try
+    {
+        System.Console.WriteLine("Введите число b:");
+        b = int.Parse(Console.ReadLine() ?? "");
+        break;
+
+    }
+    catch (Exception exc)
+    {
+        System.Console.WriteLine($"Ошибка ввода данных!!! {exc.Message}");
+    }
+}
+while (true)
+{
+    try
+    {
+        System.Console.WriteLine("Введите число c:");
+        c = int.Parse(Console.ReadLine() ?? "");
+        break;
+
+    }
+    catch (Exception exc)
+    {
+        System.Console.WriteLine($"Ошибка ввода данных!!! {exc.Message}");
+    }
+}
+bool num = a == b && b == c; //&& a > b;
+//System.Console.WriteLine(a == b);
+if (num) System.Console.WriteLine($"Числа {a} , {b}  и {c} равны.");
+else System.Console.WriteLine($"Числа {a} , {b}  и {c} не равны.");
+System.Console.WriteLine("Условие -" + num);

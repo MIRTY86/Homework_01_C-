362041b50037cddbfda519ed7cbf7d628c463517
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-15 00:55:40 +0300

    Сложение всех цифр в числе

---
+++ Task_018/Program.cs
@@ -0,0 +1,36 @@
+﻿int B = CheckingTheEnteredNumber();
+if(B==0) return;
+int sumnum = SummationOfNumbers(B);
+System.Console.WriteLine($"Сумма всех цифр в числе {B} равна --> {sumnum}.");
+
+int CheckingTheEnteredNumber()
+{
+    Console.Write("Введите число: ");
+    string a = Console.ReadLine() ?? "";
+    bool number = int.TryParse(a, out int b);
+
+    if (number)
+    {
+        Console.WriteLine("Число принято!!!");
+    }
+    else
+    {
+        Console.WriteLine("Неверный формат числа!!!");
+        return(0);
+    }
+    return Math.Abs(b);
+}
+int SummationOfNumbers(int number)
+{
+    int sum = 0;
+    if (number > 10)
+    {
+        while (number > 0)
+        {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+    }
+    else sum = sum + number % 10;
+    return sum;
+}

8bba075940fe7445007b00218fee56ac37f1e535
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-17 15:23:26 +0300

    Задача: сумма элементов массива, стоящих на четных позициях

---
+++ Task_025/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] array = GetRandomArray(5, 1, 20);
+int sumnumposindx = GetSumPositivIndexArray(array);
+Console.WriteLine($"[{String.Join(",", array)}] -> {sumnumposindx}.");
+
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+
+    return result;
+}
+
+int GetSumPositivIndexArray(int[] array)
+{
+    int sumnumposindx = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sumnumposindx = sumnumposindx + array[i];
+        }
+    }
+    return sumnumposindx;
+}
+
+
+
+
+

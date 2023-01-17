0c7eff7557d1fcf48fdaa7ea86bc603ba6afc735
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-17 16:26:35 +0300

    Задача: разница между максимальным и минимальным элементом массива

---
+++ Task_026/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] array = GetRandomArray(10, 10, 100);
+
+int diff = DifferenceMaxMinElementsArray(MaxElementsArray(array), MinElementsArray(array));
+Console.WriteLine($"[{String.Join(",", array)}] --> {diff}.");
+
+/*******************************************************************************/
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue);
+    }
+
+    return result;
+}
+
+int MaxElementsArray(int[] array)
+{
+    int maxValue = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maxValue < array[i]) maxValue = array[i];
+    }
+    System.Console.WriteLine("max element ->" + maxValue);
+    return maxValue;
+}
+
+int MinElementsArray(int[] array)
+{
+    int minValue = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (minValue > array[i]) minValue = array[i];
+    }
+    System.Console.WriteLine("min element ->" + minValue);
+    return minValue;
+}
+
+int DifferenceMaxMinElementsArray(int a, int b)
+{
+    int diff = a - b;
+    return diff;
+}

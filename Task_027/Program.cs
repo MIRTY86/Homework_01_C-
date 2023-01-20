d5c6dc2957c1271f0e1bfc1b8ffeb4db725a9dbe
Dmitriy Linov <Dimalinov@mail.ru>
2023-01-20 23:43:43 +0300

    Проверка чисел на условие > 0, введенных пользователем

---
+++ Task_027/Program.cs
@@ -0,0 +1,64 @@
+﻿int Num = GetArrayValue();
+double[] array = GetUserNumbersArray(Num);
+int value = GetArrayCheck(array);
+System.Console.WriteLine($"[{String.Join(".", array)}] --> {value} ");
+
+
+int GetArrayValue()
+{
+    int N = 0;
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество проверяемых чисел : ");
+            string userN = Console.ReadLine() ?? "";
+            N = int.Parse(userN);
+            break;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Введено неверное значение!{exc.Message}");
+        }
+
+    }
+    return N;
+}
+
+double[] GetUserNumbersArray(int N)
+{
+    double num = 0;
+    double[] usarray = new double[N];
+    for (int i = 0; i < usarray.Length; i++)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Введите проверяемое число : ");
+                string userN = Console.ReadLine() ?? "";
+                num = double.Parse(userN);
+                break;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"Введено неверное значение!{exc.Message}");
+            }
+
+        }
+        usarray[i] = num;
+    }
+    return usarray;
+}
+
+int GetArrayCheck(double[] array)
+{
+    int nunpositive = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0)
+        {
+            nunpositive ++;
+        }
+        return nunpositive;
+}
+
